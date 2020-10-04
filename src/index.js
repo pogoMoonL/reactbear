60ebca16c0a195a61493addb469a7320ccbea634
yaodehuang <sef1548@gmail.com>
2020-10-04 22:05:38 +0800

    feat: init react router

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+    <Router>
+     
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <App />
+          </Route>
+        </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
